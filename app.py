0a8e6ac643e63fa1d14ba4bd0b1959d18ed47d1c
Rajraushan Kumar <rajraushankumar360@gmail.com>
2026-04-07 12:27:13 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -243,4 +243,5 @@ def cleanup():
 threading.Thread(target=cleanup, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
